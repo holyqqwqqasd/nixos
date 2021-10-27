03b4e7323a0197b115fea5e4f8648717c535ed53
Karen <git@kapehh.net>
2021-10-28 01:17:57 +0300

    ff

--- configuration.grub.nix
+++ configuration.grub.nix
@@ -35,8 +35,6 @@
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.enp5s0.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
