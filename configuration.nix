f69f387edbae7033c5c305fb22559e4bf4ca0711
Karen <git@kapehh.net>
2021-10-29 01:57:57 +0300

    add hotkeys

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,5 @@
+# Hotkeys window manager https://i3wm.org/docs/refcard.html
+
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
