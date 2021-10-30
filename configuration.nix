8576aae947a43499d17279163a6c6c9b2823d3a7
Karen <git@kapehh.net>
2021-10-30 03:51:51 +0300

    simplify

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  environment.pathsToLink = [ "/libexec" ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
@@ -21,31 +20,8 @@
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
 
-  # boot.kernelParams = [
-  #   "console=ttyS0,115200"
-  #   "console=tty1"
-  #   "console=tty2"
-  # ];
-
-  # virtualisation.docker.enable = true;
-
-  # networking.hostName = "mark_nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.wireless.networks.Mark_wifi_5G.psk = "67680702_mscr";
-  # networking.wireless.interfaces = [ "wlp4s0" ];
-
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  # networking.useDHCP = false;
-  # networking.interfaces.ens33.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -80,14 +56,6 @@
     xkbOptions = "grp:caps_toggle,grp_led:caps";
     xkbVariant = "winkeys";
   };
-  
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   sound.enable = true;
@@ -96,18 +64,6 @@
     support32Bit = true;
     package = pkgs.pulseaudioFull;
   };
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa = {
-  #     enable = true;
-  #     support32Bit = true;
-  #   };
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.karen = {
@@ -121,14 +77,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    firefox
-    htop
-    git
-    tree
     rxvt_unicode
-    nano
+    htop
+    mc
+    git
+    google-chrome-stable
   ];
   nixpkgs.config.allowUnfree = true;
 
