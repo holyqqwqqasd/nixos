a689b0805e8e03e265520e4fb780e2e08e6ac987
Karen <git@kapehh.net>
2021-11-01 22:08:11 +0300

    fix

--- configuration.nix
+++ configuration.nix
@@ -69,18 +69,6 @@
       dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
     ];
   };
-  wayland.windowManager.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true ;
-  };
-  home.packages = with pkgs; [
-    swaylock
-    swayidle
-    wl-clipboard
-    mako # notification daemon
-    alacritty # Alacritty is the default terminal in the config
-    dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-  ];
 
   # Enable sound.
   sound.enable = true;
