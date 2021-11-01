e0201a21840eb312e48f6c213a8d70df46abdf23
Karen <git@kapehh.net>
2021-11-01 23:14:25 +0300

    ff

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Hotkeys window manager https://i3wm.org/docs/refcard.html
+# i3wm hotkeys https://i3wm.org/docs/refcard.html
 
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
@@ -31,63 +31,41 @@
   };
 
   # Enable the X11 windowing system.
-  # services.xserver = {
-  #   enable = true;
-  #   autorun = false;             # for manual run
-  #   desktopManager = {
-  #     xterm.enable = false;
-  #   };
-  #   displayManager = {
-  #     startx.enable = true;      # for manual run
-  #     defaultSession = "none+i3";
-  #   };
-  #   windowManager.i3 = {
-  #     enable = true;
-  #     extraPackages = with pkgs; [
-  #       dmenu
-  #       i3status
-  #       i3lock
-  #       i3blocks
-  #       xclip
-  #     ];
-  #   };
-  #   xkbModel = "microsoft";
-  #   layout = "us,ru(winkeys)";
-  #   xkbOptions = "grp:caps_toggle,grp_led:caps";
-  #   xkbVariant = "winkeys";
-  # };
+  services.xserver = {
+    enable = true;
+    autorun = false;             # for manual run
+    desktopManager = {
+      xterm.enable = false;
+    };
+    displayManager = {
+      startx.enable = true;      # for manual run
+      defaultSession = "none+i3";
+    };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+        xclip
+      ];
+    };
+    xkbModel = "microsoft";
+    layout = "us,ru(winkeys)";
+    xkbOptions = "grp:caps_toggle,grp_led:caps";
+    xkbVariant = "winkeys";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.zsh.enable = true;
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      swaylock
-      swayidle
-      wl-clipboard
-      mako # notification daemon
-      alacritty # Alacritty is the default terminal in the config
-      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-    ];
-  };
-  programs.fish = {
-    enable = true;
-    loginShellInit = ''
-      if test (id --user $USER) -ge 1000 && test (tty) = "/dev/tty1"
-        exec sway
-      end
-    '';
-  };
+  programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.karen = {
     isNormalUser = true;
     extraGroups = [ "wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "sway" "docker" ]; # Enable ‘sudo’ for the user.
-    # shell="/run/current-system/sw/bin/zsh";
-    shell = pkgs.fish;
+    shell="/run/current-system/sw/bin/zsh";
   };
 
   nix.trustedUsers = [ "root" "karen" ];
