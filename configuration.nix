3bb345de4c2b3e21eb558630cfe509b630a2a413
Karen <git@kapehh.net>
2021-11-01 21:53:21 +0300

    test sway

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,31 @@
     xkbVariant = "winkeys";
   };
 
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      mako # notification daemon
+      alacritty # Alacritty is the default terminal in the config
+      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+    ];
+  };
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true ;
+  };
+  home.packages = with pkgs; [
+    swaylock
+    swayidle
+    wl-clipboard
+    mako # notification daemon
+    alacritty # Alacritty is the default terminal in the config
+    dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+  ];
+
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio = {
