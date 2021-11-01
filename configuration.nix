c7b1704fc8ef50cc06843c5caa3bfee739e6635f
Karen <git@kapehh.net>
2021-11-01 23:31:18 +0300

    wut

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,13 @@
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
 
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
+
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
 
