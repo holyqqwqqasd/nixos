acbe7d2f028ba64e6a544b15b0df69c4f1d9ee16
Karen <git@kapehh.net>
2021-11-02 22:28:48 +0300

    fix

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,3 @@
-# i3wm hotkeys https://i3wm.org/docs/refcard.html
-
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -31,14 +25,15 @@
   };
 
   # Enable the X11 windowing system.
+  # i3wm hotkeys https://i3wm.org/docs/refcard.html
   services.xserver = {
     enable = true;
-    autorun = false;             # for manual run
+    autorun = false;
     desktopManager = {
       xterm.enable = false;
     };
     displayManager = {
-      startx.enable = true;      # for manual run
+      startx.enable = true;
       defaultSession = "none+i3";
     };
     windowManager.i3 = {
@@ -57,14 +52,12 @@
     xkbVariant = "winkeys";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.karen = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "sway" "docker" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "docker" ];
     shell="/run/current-system/sw/bin/zsh";
   };
 
@@ -87,24 +80,11 @@
     mc
     git
     google-chrome
+    tdesktop
   ];
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "21.05";
 }
