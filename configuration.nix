6f7fa135897e363b220de4a0cb6080d446112a2c
Karen <main@kapehh.net>
2022-04-10 18:03:01 +0300

    Add audio

--- configuration.nix
+++ configuration.nix
@@ -46,6 +46,7 @@
 
   # Graphics
   services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
+  # hardware.opengl.enable = true;
   hardware.opengl = {
     enable = true;
     driSupport = true;
@@ -63,6 +64,20 @@
   # Enable sound.
   # sound.enable = true;
   # hardware.pulseaudio.enable = true;
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
